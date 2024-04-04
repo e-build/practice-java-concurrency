a496322768ce736e1a54c465efa33d0ec73fba97
e-build <donggeol2@kakao.com>
2024-04-05 08:48:22 +0900

    add ch03 ex01

---
+++ src/main/java/me/practice/concurrency/ch_04/ex_01_exception_handling/CatchException.java
@@ -0,0 +1,20 @@
+package me.practice.concurrency.ch_04.ex_01_exception_handling;
+
+public class CatchException {
+
+    public static void main(String[] args) {
+
+        try {
+            new Thread(() -> {
+                throw new RuntimeException("스레드 1 예외!");
+            }).start();
+        }catch(Exception e){
+            sendNotificationToAdmin(e);
+        }
+
+    }
+
+    private static void sendNotificationToAdmin(Throwable e) {
+        System.out.println("관리자에게 알림: " + e.getMessage());
+    }
+}
