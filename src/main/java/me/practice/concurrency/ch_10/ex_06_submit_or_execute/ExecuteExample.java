2737cb4dffbd6530c7d3810c44d1f3f213261cb1
e-build <donggeol2@daum.net>
2024-04-17 00:02:01 +0900

    add ch10 ex_06 submit or execute

---
+++ src/main/java/me/practice/concurrency/ch_10/ex_06_submit_or_execute/ExecuteExample.java
@@ -0,0 +1,19 @@
+package me.practice.concurrency.ch_10.ex_06_submit_or_execute;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecuteExample {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        // Runnable을 사용한 작업 실행 (execute 메서드)
+        executorService.execute(() -> System.out.println("Runnable 작업 실행"));
+        // 스레드 풀 종료
+        executorService.shutdown();
+
+        new Thread(() -> System.out.println("Runnable 작업 실행")).start();
+
+    }
+}
