4aabc5d53c385458df8771a1656372ced5771d5a
e-build <donggeol2@daum.net>
2024-04-20 15:23:21 +0900

    add ch11 ex_02 CompletableFutureExample & sync block async non-block readme

--- src/main/java/me/practice/concurrency/ch_11/ex_01_sync_block/AsyncBlocking.java
+++ src/main/java/me/practice/concurrency/ch_11/ex_01_sync_block/AsyncBlocking.java
@@ -14,18 +14,20 @@ public class AsyncBlocking {
         // 비동기 & 논블록킹
         Future<String> future = executor.submit(task);
 
-        // 블록킹, 이 구문은 맨 마지막에 위치 해도 무방함
-        try {
-            String result = future.get();
-            System.out.println("블록킹 작업 결과: " + result);
-        } catch (InterruptedException | ExecutionException e) {
-        }
-
         // 메인 작업 수행
         int sum = 0;
         for (int i = 0; i < 1000000; i++) {
             sum = sum + i;
         }
+        System.err.println("메인 작업 결과: " + sum);
+
+        // 블록킹, 이 구문은 맨 마지막에 위치 해도 무방함
+        try {
+            String result = future.get();
+            System.err.println("블록킹 작업 결과: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+        }
         executor.shutdown();
+        System.err.println("프로세스 종료");
     }
 }
